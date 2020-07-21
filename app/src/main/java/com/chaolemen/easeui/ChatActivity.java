b33c9b35727b4862dd88d655b1673ee80eded2d8
chaolemen-github <2863346378@qq.com>
2020-07-21 15:00:38 +0800

    easeUi自动生成

---
+++ app/src/main/java/com/chaolemen/easeui/ChatActivity.java
@@ -0,0 +1,23 @@
+package com.chaolemen.easeui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hyphenate.easeui.ui.EaseChatFragment;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private EaseChatFragment chatFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        //use EaseChatFratFragment
+        chatFragment = new EaseChatFragment();
+        //pass parameters to chat fragment
+        chatFragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().add(R.id.container, chatFragment).commit();
+    }
+}
---
+++ app/src/main/java/com/chaolemen/easeui/utils/ResultCallBack.java
@@ -0,0 +1,6 @@
+package com.chaolemen.easeui.utils;
+
+public interface ResultCallBack {
+    void onSuccess(String filePath, long duration);
+    void onFail(String str);
+}
